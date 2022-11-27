39a1f494b6b8dca34aa522c620aaea830a1c69a3
apisdriven <apisdriven@gmail.com>
2022-11-27 18:55:12 -0500

    Fixed icon display

--- Assets/script.js
+++ Assets/script.js
@@ -26,6 +26,7 @@ function getWeather(city) {
         storedCityList.appendChild(newStoredCity)
 
         // Target Current Conditions
+        var currentIcon = document.getElementById("icon")
         var currentDate = document.getElementById("curr-date")
         var currentTemp = document.getElementById("temp")
         var currentWind = document.getElementById("wind")
@@ -34,6 +35,7 @@ function getWeather(city) {
         // Populate Current Conditions
         var myDate = new Date( data.dt *1000);
         currentDate.innerHTML = myDate;
+        currentIcon.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`
         currentTemp.innerHTML = data.main.temp + " degrees"
         currentWind.innerHTML = data.wind.speed + " mph"
         currentHumid.innerHTML = data.main.humidity + "%"
@@ -58,7 +60,7 @@ function getWeather(city) {
         .then((response) => response.json())
         .then((data) => {
             for (let i = 3; i < data.list.length; i += 8) {
-                // console.log(data.list[i]);
+                console.log(data.list[i]);
                 let forecast = data.list[i];
                 // pnpforecast.dt_txt
                 
@@ -76,11 +78,12 @@ function getWeather(city) {
                 cardEl.appendChild(dayDate)
                 dayDate.innerHTML = forecast.dt_txt
                 
-                var dayIcon = document.createElement("div")
+                var dayIcon = document.createElement("img")
                 dayIcon.setAttribute("class", "dayIcon")
+                dayIcon.setAttribute("src", "")
                 cardEl.appendChild(dayIcon)
                 // need to figure out how to add icon
-                dayIcon.innerHTML = "img"
+                dayIcon.src = `http://openweathermap.org/img/w/${forecast.weather[0].icon}.png`
                 
                 var weatherStats = document.createElement("div")
                 cardEl.appendChild(weatherStats)
